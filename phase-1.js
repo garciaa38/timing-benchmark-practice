566f703cbf0cb2ffe12ede00812c3d93f795af49
Bobby Naluz <bnaluz9@gmail.com>
2023-10-17 20:35:44 -0400

    phase2 complete

--- phase-1.js
+++ phase-1.js
@@ -1,27 +1,30 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
   // Fill this in
-
+  // console.time('addNums');
   let total = 0;
   for (let i = 0; i <= n; i++) {
     total += i;
   }
+  // console.timeEnd('addNums');
   return total;
-
 }
 
+// addNums(100000);
 
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
   // Fill this in
   let total = 0;
+  // console.time('addManyNums');
   for (let i = 0; i <= n; i++) {
-    total += addNums(i);
+    //o(n)
+    total += addNums(i); //o(n) === o(n^2)
   }
+  // console.timeEnd('addManyNums');
   return total;
-
 }
 
-
+// addManyNums(100000);
 
 module.exports = [addNums, addManyNums];
--- phase-2.js
+++ phase-2.js
@@ -1,10 +1,12 @@
-const [addNums, addManyNums] = require("./phase-1");
+const [addNums, addManyNums] = require('./phase-1');
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
   let newArr = [];
+
   let num = increment;
+
   for (let i = 0; i < 10; i++) {
     newArr.push(addNums(num));
     num += increment;
@@ -15,7 +17,17 @@ function addNums10(increment) {
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
-
+  let newArr = [];
+  let num = increment;
+  console.time('addManyNums10');
+  for (let i = 0; i < 10; i++) {
+    newArr.push(addManyNums(num));
+    num += increment;
+  }
+  console.timeEnd('addManyNums10');
+  return newArr;
 }
+
+// addManyNums10(10000);
 
 module.exports = [addNums10, addManyNums10];
